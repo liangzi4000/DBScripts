17f40c7605a86fbae1025058399721241e4006a1
liangzi4000 <brucesgp@gmail.com>
2018-03-16 15:09:12 +0800

    Full mode database backup script

---
+++ Full Mode DB Backup Scripts/ExecFullDatabaseBackup.cmd
@@ -0,0 +1,2 @@
+set datetimef=%date:~10,4%%date:~4,2%%date:~7,2%%time:~0,2%%time:~3,2%%time:~6,2%
+SQLCMD -S "(local)" -d NUH_OTRS -U medisys -P medinno -i "FullDatabaseBackup.sql" -o "%datetimef%.rpt"
