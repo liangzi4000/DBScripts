f924a804170a0d8b1fe3f0a6b68ad37ba48b37f9
Bruce Ye <brucesgp@gmail.com>
2019-06-30 04:46:07 +0800

    .

---
+++ 20. Execute a dtsx file with parameters.bat
@@ -0,0 +1,4 @@
+rem "C:\Program Files (x86)\Microsoft SQL Server\140\DTS\Binn\DTExec.exe" /FILE "D:\Test\Package.dtsx" /SET \Package.Variables[User::DestinationFile].Properties[Value];"D:\Test\dest.txt" /SET \Package.Variables[User::SourceFile].Properties[Value];"D:\Test\source.txt" 
+
+"C:\Program Files (x86)\Microsoft SQL Server\140\DTS\Binn\DTExec.exe" /FILE "D:\Test\Package.dtsx"
+
