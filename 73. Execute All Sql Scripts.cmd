0b3d47f07b536006ffa6329ced0f7b0fd9b1385a
Bruce Ye <brucesgp@gmail.com>
2020-10-29 23:20:23 +0800

    .

--- 73. Execute All Sql Scripts.cmd
+++ 73. Execute All Sql Scripts.cmd
@@ -1,7 +1,10 @@
 @ECHO OFF
+set resultFileName=result.txt
 for /f "usebackq delims=" %%i in (`dir *.sql /s /b`) do (
 rem  echo %%i.csv
-  SQLCMD -S "." -d piSHaRe_20161206 -i "%%i" -o "%%i.rpt"
+  SQLCMD -I -S "." -d CargoClique_Kiosk -i "%%i" -o "%%i.rpt"
+  Echo ------------------------------- "%%i.rpt" ------------------------------- >> %resultFileName%
+  type "%%i.rpt" >> %resultFileName%
   Echo Completed Script: %%i
 )
 pause
